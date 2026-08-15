06c0f478ae4fc3bd188df4848640f87e201b259d
AFIF ZILANI <afifzilani4566@gmail.com>
2026-08-16 00:26:31 +0600

    feat(sales): add Overview tab as the default landing tab

--- src/pages/sales/sales-page.tsx
+++ src/pages/sales/sales-page.tsx
@@ -1,22 +1,26 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { usePageTitle } from "@/components/layout/use-page-title";
+import { OverviewTab } from "@/pages/sales/overview-tab";
 import { RegularSalesTab } from "@/pages/sales/regular-sales-tab";
 import { BirdSalesTab } from "@/pages/sales/bird-sales-tab";
 
 export function SalesPage() {
   usePageTitle("Sales");
-
   return (
-    <Tabs defaultValue="regular">
+    <Tabs defaultValue="overview">
       <TabsList>
-        <TabsTrigger value="regular">Regular Sales</TabsTrigger>
+        <TabsTrigger value="overview">Overview</TabsTrigger>
         <TabsTrigger value="birds">Bird Sales</TabsTrigger>
+        <TabsTrigger value="regular">Regular Sales</TabsTrigger>
       </TabsList>
-      <TabsContent value="regular">
-        <RegularSalesTab />
+      <TabsContent value="overview">
+        <OverviewTab />
       </TabsContent>
       <TabsContent value="birds">
         <BirdSalesTab />
+      </TabsContent>
+      <TabsContent value="regular">
+        <RegularSalesTab />
       </TabsContent>
     </Tabs>
   );
